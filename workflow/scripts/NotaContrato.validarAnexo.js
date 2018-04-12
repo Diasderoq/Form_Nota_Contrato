0e1bd1eeaaaf155f98eb1f761f814fcb53024c69
Diasderoq <35240279+Diasderoq@users.noreply.github.com>
2018-04-12 09:26:48 -0300

    Delete NotaContrato.validarAnexo.js

--- workflow/scripts/NotaContrato.validarAnexo.js
+++
@@ -1,8 +0,0 @@
-function validarAnexo(){
-	var docs = hAPI.listAttachments();
-	log.info("[validarAnexo] docs.size:" + docs.size());
-	if(docs.size() == 0){
-		throw " Anexo Obrigatório";
-	}
-	return false;
-}
